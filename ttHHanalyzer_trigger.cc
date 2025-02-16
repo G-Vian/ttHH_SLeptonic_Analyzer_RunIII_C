38f6fa4585537e72663a8875d902d39f3cf2dc62
Gabriel V. Vian <gabriel.vian@unesp.br>
2025-02-15 23:01:25 -0300

    Update ttHHanalyzer_trigger.cc

--- ttHHanalyzer_trigger.cc
+++ ttHHanalyzer_trigger.cc
@@ -114,7 +114,7 @@ void ttHHanalyzer::createObjects(event * thisEvent, sysName sysType, bool up){
 				 _ev->Flag_BadPFMuonFilter ||
 				 _ev->Flag_eeBadScFilter ||
 				 _ev->Flag_ecalBadCalibFilter);
-	    if(_sample == "ee"){
+	    if(_sampleName == "ee"){
 		thisEvent->setTrigger((_ev->HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL ||
 				       _ev->HLT_Ele23_Ele12_CaloIdL_TrackIdL_IsoVL_DZ) // ||
 				      // _ev->HLT_Ele27_WPTight_Gsf)
@@ -122,7 +122,7 @@ void ttHHanalyzer::createObjects(event * thisEvent, sysName sysType, bool up){
 					   _ev->HLT_Mu17_TrkIsoVVL_Mu8_TrkIsoVVL_DZ_Mass3p8));// ||
 					   //_ev->HLT_IsoMu24_eta2p1 ||
 					   //_ev->HLT_IsoMu27));
-	    } else if(_sample == "emu"){ 
+	    } else if(_sampleName == "emu"){ 
 		thisEvent->setTrigger((_ev->HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL ||
 				       _ev->HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_DZ ||
 				       _ev->HLT_Mu12_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_DZ ||
