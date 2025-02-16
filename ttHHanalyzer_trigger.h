f93cdd0933ef8ce64f24c465e11adff57fb7b010
Gabriel V. Vian <gabriel.vian@unesp.br>
2025-02-15 22:49:18 -0300

    Update ttHHanalyzer_trigger.h

--- ttHHanalyzer_trigger.h
+++ ttHHanalyzer_trigger.h
@@ -102,7 +102,8 @@ class objectPhysics {
  private:
     TLorentzVector _p4;
     float _pxOffset = 0., _pyOffset = 0., _pzOffset = 0., _EOffset = 0.;
-    int _year;
+    _year = year;
+    std::string _year;
 };
 
 
