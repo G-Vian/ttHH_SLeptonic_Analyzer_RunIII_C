192382b635d68529061ae82c0860fa569c293f8c
Gabriel V. Vian <gabriel.vian@unesp.br>
2025-06-11 10:34:17 -0300

    Update Ploter3.cpp

--- Ploter3.cpp
+++ Ploter3.cpp
@@ -429,14 +429,15 @@ void Iteration_Directories_And_Histograms(const unordered_map<string, ProcessInf
 
 int Ploter3() {
     unordered_map<string, ProcessInfo> processes = {
-{"TTSL", {"TTSL.root", false, kOrange, "TTSL"}},
-        {"TTZ", {"TTZ.root", false, kCyan, "TTZ"}},
-        {"TTH", {"TTHTobb.root", false, kMagenta, "TTH"}},
-        {"TT4b", {"TT4b.root", false, kGreen, "TT4b"}},
-        {"TTZH", {"TTZH.root", false, kBlue, "TTZH"}},
-        {"TTZZ", {"TTZZ.root", false, kRed, "TTZZ"}},
-        {"ttHH", {"ttHH.root", true, kBlack, "ttHH"}},
-    };
+{"TTSL", {"TTSL.root", false, kOrange+1, "TTSL"}},
+//{"TTZ", {"TTZ.root", false, kAzure+1, "TTZ"}},
+{"TTH", {"TTH.root", false, kPink+1, "TTH"}},
+{"TT4b", {"TT4b.root", false, kSpring+3, "TT4b"}},
+{"TTZH", {"TTZH.root", false, kTeal+1, "TTZH"}},
+{"TTZZ", {"TTZZ.root", false, kRed+1, "TTZZ"}},
+{"TTHH", {"TTHH.root", true, kBlack, "TTHH"}},
+        // Add other processes as needed
+ };
 
     // Define histogram settings with binning options
    unordered_map<string, vector<HistogramSetting>> histogramSettings = {
